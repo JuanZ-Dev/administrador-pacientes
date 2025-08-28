29c8d56bc167af7593f775082f7429c9ccbb6980
JuanZ-Dev <juan.z.981611@gmail.com>
2025-08-28 00:22:51 -0500

    add actions to the update and delete patient buttons

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,3 +1,4 @@
+import { usePatientStore } from "../store";
 import type { Patient } from "../types";
 import PatientDetailItem from "./PatientDetailItem";
 
@@ -6,6 +7,9 @@ type PatientDetailsProps = {
 };
 
 export default function PatientDetails({ patient }: PatientDetailsProps) {
+  const deletePatient = usePatientStore((state) => state.deletePatient);
+  const getPatientById = usePatientStore((state) => state.getPatientById);
+
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
       <PatientDetailItem label="Id" data={patient.id} />
@@ -15,10 +19,11 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
       <PatientDetailItem label="Fecha Alta" data={patient.date.toString()} />
       <PatientDetailItem label="Sintomas" data={patient.symptoms} />
 
-      <div className="flex justify-around mt-10">
+      <div className="flex flex-col lg:flex-row gap-3 justify-around mt-10">
         <button
           type="button"
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 hover:cursor-pointer text-white font-bold uppercase rounded-lg "
+          onClick={() => getPatientById(patient.id)}
         >
           Editar
         </button>
@@ -26,6 +31,7 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 hover:cursor-pointer text-white font-bold uppercase rounded-lg"
+          onClick={() => deletePatient(patient.id)}
         >
           Eliminar
         </button>
